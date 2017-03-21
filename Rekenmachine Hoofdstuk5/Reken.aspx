d543cfa172dc1e1932557d2054e4bb7de1571ded
jordyortega <jordy-9999@hotmail.com>
2017-03-21 11:58:42 +0100

    Add files via upload

---
+++ Rekenmachine Hoofdstuk5/Reken.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reken.aspx.cs" Inherits="Rekenmachine_Hoofdstuk5.Reken" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <asp:Label ID="lblGetal1" runat="server" Text="Getal 1"></asp:Label>&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtGetal1" runat="server" OnTextChanged="txtGetal1_TextChanged"></asp:TextBox>*
+        <asp:RequiredFieldValidator ID="rfvGetal1" runat="server" ControlToValidate="txtGetal1" ErrorMessage="Vul getal 1 in!" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:RangeValidator ID="rvGetal1" runat="server" ControlToValidate="txtGetal1" ErrorMessage="Tussen de 1 en 100" MaximumValue="100" MinimumValue="1" Display="Dynamic" Type="Double"></asp:RangeValidator>
+        <asp:Button ID="btnOptel" runat="server" Text="Optellen" OnClick="btnOptel_Click" />
+        <br />
+        <asp:Label ID="lblGetal2" runat="server" Text="Getal 2"></asp:Label>&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="txtGetal2" runat="server"></asp:TextBox>*
+        <asp:RequiredFieldValidator ID="rfvGetal2" runat="server" ControlToValidate="txtGetal2" ErrorMessage="Vul getal 2 in!" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:RangeValidator ID="RangeValidator2" runat="server" ControlToValidate="txtGetal2" ErrorMessage="Tussen de 1 en 100" MaximumValue="100" MinimumValue="1" Display="Dynamic" Type="Double"></asp:RangeValidator>
+        <asp:Button ID="btnAftrek" runat="server" Text="Aftrekken" OnClick="btnAftrek_Click" />
+        <br />
+        <asp:Label ID="lblUitkomst" runat="server" Text="Uitkomst"></asp:Label>
+
+    &nbsp;<asp:TextBox ID="txtUitkomst" runat="server"></asp:TextBox>
+
+        <br />
+        <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToCompare="txtGetal1" ControlToValidate="txtGetal2" Display="Dynamic" ErrorMessage="Vul dezelfde getallen in!"></asp:CompareValidator>
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Telefoonnummer"></asp:Label>
+&nbsp;<asp:TextBox ID="txtTelefoon" runat="server"></asp:TextBox>
+        <br />
+        <asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="txtTelefoon" Display="Dynamic" ErrorMessage="Vul een geldig nummer in! Zoals: 074 2780013 of 0742780013" ValidationExpression="(((\d{3}) (\d{7}))|(\d{10}))"></asp:RegularExpressionValidator>
+
+    </form>
+</body>
+</html>
---
+++ Rekenmachine Hoofdstuk5/Reken.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Rekenmachine_Hoofdstuk5
+{
+    public partial class Reken : System.Web.UI.Page
+    {
+
+        decimal get1;
+        decimal get2;
+        decimal uitkomst;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnOptel_Click(object sender, EventArgs e)
+        {
+            // Valideren dat ingevoerde teken een cijfer is
+           /* if(!txtGetal1.Text.Contains(",") && !txtGetal1.Text.Contains("0") && !txtGetal1.Text.Contains("1") && !txtGetal1.Text.Contains("2") && !txtGetal1.Text.Contains("3")
+                && !txtGetal1.Text.Contains("4") && !txtGetal1.Text.Contains("5") && !txtGetal1.Text.Contains("6")
+                && !txtGetal1.Text.Contains("7") && !txtGetal1.Text.Contains("8") && !txtGetal1.Text.Contains("9")
+                ||
+                !txtGetal2.Text.Contains(",") && !txtGetal2.Text.Contains("0") && !txtGetal2.Text.Contains("1") && !txtGetal2.Text.Contains("2") && !txtGetal2.Text.Contains("3")
+                && !txtGetal2.Text.Contains("4") && !txtGetal2.Text.Contains("5") && !txtGetal2.Text.Contains("6")
+                && !txtGetal2.Text.Contains("7") && !txtGetal2.Text.Contains("8") && !txtGetal2.Text.Contains("9"))
+            {
+                txtUitkomst.Text = "Alleen cijfers of komma!";
+                return;
+            }*/
+
+            get1 = Convert.ToDecimal(txtGetal1.Text);
+            get2 = Convert.ToDecimal(txtGetal2.Text);
+            uitkomst = get1 + get2;
+            txtUitkomst.Text = Convert.ToString(uitkomst);
+        }
+
+        protected void btnAftrek_Click(object sender, EventArgs e)
+        {
+            // Valideren dat ingevoerde teken een cijfer is
+           /* if (!txtGetal1.Text.Contains(",") && !txtGetal1.Text.Contains("0") && !txtGetal1.Text.Contains("1") && !txtGetal1.Text.Contains("2") && !txtGetal1.Text.Contains("3")
+                && !txtGetal1.Text.Contains("4") && !txtGetal1.Text.Contains("5") && !txtGetal1.Text.Contains("6")
+                && !txtGetal1.Text.Contains("7") && !txtGetal1.Text.Contains("8") && !txtGetal1.Text.Contains("9")
+                ||
+                !txtGetal2.Text.Contains(",") && !txtGetal2.Text.Contains("0") && !txtGetal2.Text.Contains("1") && !txtGetal2.Text.Contains("2") && !txtGetal2.Text.Contains("3")
+                && !txtGetal2.Text.Contains("4") && !txtGetal2.Text.Contains("5") && !txtGetal2.Text.Contains("6")
+                && !txtGetal2.Text.Contains("7") && !txtGetal2.Text.Contains("8") && !txtGetal2.Text.Contains("9"))
+            {
+                txtUitkomst.Text = "Alleen cijfers of komma!";
+                return;
+            }*/
+
+            get1 = Convert.ToDecimal(txtGetal1.Text);
+            get2 = Convert.ToDecimal(txtGetal2.Text);
+            uitkomst = get1 - get2;
+            txtUitkomst.Text = Convert.ToString(uitkomst);
+        }
+
+        protected void txtGetal1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
