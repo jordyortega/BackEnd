1bcfe9ff342b3598e54b8fff6b10ea2b68ee77c8
jordyortega <jordy-9999@hotmail.com>
2017-05-23 11:12:05 +0200

    Add files via upload

---
+++ priemgetal/default.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="priemgetal._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <b>Exercise 1</b><br />
+    
+        Number:
+        <asp:TextBox ID="numberBox" runat="server"></asp:TextBox>
+        <asp:Button ID="btnCheck" runat="server" OnClick="btnCheck_Click" Text="Check" />
+        <asp:RangeValidator ID="RangeValidatorPrimeNumber" runat="server" ControlToValidate="numberBox" Display="Dynamic" ErrorMessage="The number should have a minimum value of 2" MaximumValue="2147483647" MinimumValue="2" Type="Integer"></asp:RangeValidator>
+    
+    </div>
+        <br />
+        <asp:Label ID="lblPriem" runat="server"></asp:Label>
+        <br />
+        <br />
+        <b>Exercise 2</b><br />
+        Maximum number: <asp:TextBox ID="maxNumberBox" runat="server"></asp:TextBox>
+        <asp:Button ID="BtnCheckMax" runat="server" OnClick="BtnCheckMax_Click" Text="Check all" />
+        <asp:RangeValidator ID="RangeValidatorPrimeMaxNumber" runat="server" ControlToValidate="maxNumberBox" Display="Dynamic" ErrorMessage="The number should have a minimum value of 2" MaximumValue="2147483647" MinimumValue="2" Type="Integer"></asp:RangeValidator>
+        <br />
+        <br />
+        <div style="width:200px;"><asp:Label ID="lblPrimeNumbers" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+        Total time:
+            <asp:Label ID="lblTotaal" runat="server"></asp:Label>
+            &nbsp;ms<br />
+            Time per iteration:
+            <asp:Label ID="lblEach" runat="server"></asp:Label>
+    &nbsp;ms<br />
+        <br />
+        <b>Exercise 3</b><br />
+        Eerste getal
+        <asp:TextBox ID="txtGetal1" runat="server"></asp:TextBox>
+        <br />
+        Tweede getal
+        <asp:TextBox ID="txtGetal2" runat="server"></asp:TextBox>
+        <asp:Button ID="ggdButton" runat="server" OnClick="ggdButton_Click" Text="Show greatest common divisor " />
+        <br />
+        <br />
+        <asp:Label ID="lblGgd" runat="server" Text=""></asp:Label>
+        <br />
+        <br />
+        <b>Exercise 4</b>
+        <br />
+        Word&nbsp;
+        <asp:TextBox ID="txtPalin" runat="server"></asp:TextBox>
+        <asp:Button ID="btnPalin" runat="server" OnClick="btnPalin_Click" Text="Yes or no" />
+        <br />
+        <br />
+        <asp:Label ID="lblPalin" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ priemgetal/default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace priemgetal
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            lblPrimeNumbers.Text = "";
+        }
+
+        public bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(numberBox.Text);
+            if (IsPrime(number))
+            {
+                lblPriem.Text = number + " is a prime number.";
+            }
+            else
+            {
+                lblPriem.Text = number + " is not a prime number.";
+            }
+        }
+
+        protected void BtnCheckMax_Click(object sender, EventArgs e)
+        {
+            int number2 = Convert.ToInt32(maxNumberBox.Text);
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 2; i <= number2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    lblPrimeNumbers.Text += i.ToString() + ", ";
+                }
+            }
+            TimeSpan totalDuration = stopwatch.Elapsed;
+            TimeSpan durationForEachIteration =
+                TimeSpan.FromTicks(totalDuration.Ticks / number2);
+
+            lblTotaal.Text = totalDuration.ToString("fffffff");
+            lblEach.Text = durationForEachIteration.ToString("fffffff");
+        }
+
+        private void berekenGGD()
+        {
+            int getal1 = Convert.ToInt32(txtGetal1.Text);
+            int getal2 = Convert.ToInt32(txtGetal2.Text);
+            int kleinste = 0;
+            int grootste = 0;
+            int GGD = 0;
+
+            if (getal1 > getal2)
+            {
+                grootste = getal1;
+                kleinste = getal2;
+            }
+            else
+            {
+                grootste = getal2;
+                kleinste = getal1;
+            }
+
+            for (int i = 0; i <= kleinste; i++)
+            {
+                if (grootste % (kleinste - i) == 0 && kleinste % (kleinste - i) == 0)
+                {
+                    GGD = kleinste - i;
+                    break;
+                }
+            }
+            lblGgd.Text = "The greatest common divisor is " + GGD.ToString();
+        }
+
+        protected void ggdButton_Click(object sender, EventArgs e)
+        {
+            berekenGGD();
+        }
+
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+        protected void btnPalin_Click(object sender, EventArgs e)
+        {
+            string word = txtPalin.Text;
+
+            if (IsPalindrome(word))
+                lblPalin.Text = "Ja, "+word+" is een Palindrome.";
+            else
+                lblPalin.Text = "Nee, " + word + " is geen Palindrome.";
+        }
+    }
+}
